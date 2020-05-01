1cf40399ee43bd4c463266f8f8f7e565aa4a6410
mohdshafi2 <mohdshafiuddin2@gmail.com>
2020-05-01 13:00:20 +0530

    about pages

--- pages/about.js
+++ pages/about.js
@@ -2,15 +2,11 @@ import React from 'react'
 import Layout from "../src/component/Layout/Layout";
 
 const About = () => {
-
   return (
       <Layout titleTag="About Page">
-
         <main>
           <h1>About Page</h1>
         </main>
-
-
       </Layout>
   )
 }
